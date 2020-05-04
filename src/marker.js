814b5730fe010a7d7a4ba5a93147e0dc1902c122
Kristy Li <kristyli20.0@gmail.com>
2020-05-04 13:48:44 -0400

    Reformat marker.js

--- src/marker.js
+++ src/marker.js
@@ -1,12 +1,12 @@
 const mapboxgl = require("mapbox-gl");
-mapboxgl.accessToken = 'pk.eyJ1IjoidGFhZWVlIiwiYSI6ImNrOXNvbjJkdjE0MzQzbXA1aWpxdnlzb2sifQ.itnwFmkju8QE0QzIElW6Tg';
-
 const map = new mapboxgl.Map({
   container: "map",
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-  zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+  center: [-74.009, 40.705],
+  zoom: 12,
+  style: "mapbox://styles/mapbox/streets-v10"
 });
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidGFhZWVlIiwiYSI6ImNrOXNvbjJkdjE0MzQzbXA1aWpxdnlzb2sifQ.itnwFmkju8QE0QzIElW6Tg';
 
 export function buildMarker(type, coordinates) {
   const divElement = document.createElement('div');
